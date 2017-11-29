e950fdd34d0d138efae9ec9de9cceee91463123d
Ricardo <ricardo.ovando@cocha.com>
2017-11-29 15:05:37 -0300

    doesnt need _id

--- models/mongo/schemas/payment.js
+++ models/mongo/schemas/payment.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     cpnr: {type: String},
     xpnr: {type: String},
     email: {type: String},
