61b181c70f6d1d4b570aec3f4ad8b6931e677ed0
Ricardo <ricardo.ovando@cocha.com>
2018-01-19 10:34:52 -0300

    change url slack channel

--- services/SlackService.js
+++ services/SlackService.js
@@ -4,7 +4,8 @@ var rp = require('request-promise');
 
 const slackLogFlag = process.env.LOG_TO_SLACK || true;
 const slackChannels = {
-    alertasvh: 'https://hooks.slack.com/services/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'
+    //alertasvh: 'https://hooks.slack.com/services/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'
+    alertasvh: 'https://hooks.slack.com/services/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'
 };
 
 async function log(level, msg, type, callback) {
